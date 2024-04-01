8033cea616b737ae3b9593e44c686dfe2b7ec322
Kelv Gooding <82043281+kelvgooding@users.noreply.github.com>
2024-04-02 00:23:51 +0100

    Update generate_config.py

--- scripts/python/generate_config.py
+++ scripts/python/generate_config.py
@@ -2,7 +2,7 @@
 Author: Kelvin Gooding
 Created: 2024-04-02
 Updated: 2024-04-02
-Version: 1.0
+Version: 1.1
 """
 
 # Modules
@@ -15,11 +15,11 @@ server = input('Enter SMTP server: ')
 mailbox = input('Enter Mailbox: ')
 password = input('Enter Mailbox Password: ')
 
-print(f'\nA new file has now been generated - {path}/config.ini')
-
-with open(f'{path}/config.ini', 'w') as f:
+with open(f'{path}/.config.ini', 'w') as f:
     f.write('[SMTP]')
     f.write(f'\nSMTP_SERVER = {server}')
     f.write(f'\nSMTP_PORT = 587')
     f.write(f'\nSMTP_EMAIL = {mailbox}')
     f.write(f'\nSMTP_PASSWORD = {password}')
+
+print(f'\nA new file has now been generated - {path}/.config.ini')
